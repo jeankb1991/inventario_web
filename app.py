24ca7cf68d2b92be6a25618582d8dc0461004c8a
jeankb1991 <86066466+jeankb1991@users.noreply.github.com>
2025-11-11 06:49:51 -0300

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -160,3 +160,4 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     print(f"🚀 Servidor rodando em http://0.0.0.0:{port}")
     app.run(host="0.0.0.0", port=port, debug=True)
+
